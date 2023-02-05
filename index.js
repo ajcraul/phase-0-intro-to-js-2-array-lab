90df423c8ac6794f046c297c8391d41a64e14aaf
allison craul <craul.aj@gmail.com>
2023-02-05 07:33:59 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name){
+    var newArray = cats.slice();
+    newArray.push("Broom");
+    return newArray;
+  }
+
+  function prependCat(name) {
+    var otherArray = cats.slice();
+    otherArray.unshift("Arnold");
+    return otherArray;
+  }
+
+  function removeLastCat() {
+    var thirdArray =cats.slice();
+    thirdArray.pop();
+    return thirdArray;
+  }
+
+  function removeFirstCat() {
+    var fourthArray = cats.slice();
+    fourthArray.shift();
+    return fourthArray;
+  }
